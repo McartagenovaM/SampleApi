78bd7cadf8302e44bb51b8d90be73b8cf6420f53
Miguel <Miguel@PRODESK-DEV>
2025-07-02 20:06:01 -0500

    fixed docker configuration

--- SampleApi/Program.cs
+++ SampleApi/Program.cs
@@ -2,6 +2,13 @@ using SampleApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//establecer el puerto para Kestrel
+//builder.WebHost.ConfigureKestrel(options =>
+//{
+//    options.ListenAnyIP(7241); // Escucha en http://0.0.0.0:7241 dentro del contenedor
+//});
+
+
 // Agregar servicios Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,11 +24,14 @@ builder.Services.AddSingleton<DummyLogService>();
 var app = builder.Build();
 
 // Configurar Swagger en tiempo de desarrollo
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 app.MapControllers();
